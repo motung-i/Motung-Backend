bb8212cfae9de113f939c3f274501e4c4ebb63a9
이현준 <11dlguswns@naver.com>
2025-03-10 15:14:16 +0900

    feat :: jackson 설정

---
+++ src/main/kotlin/kr/motung_i/backend/global/config/JacksonConfig.kt
@@ -0,0 +1,19 @@
+package kr.motung_i.backend.global.config
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import java.time.format.DateTimeFormatter
+
+@Configuration
+class JacksonConfig {
+    @Bean
+    fun jackson2ObjectMapperBuilder(): Jackson2ObjectMapperBuilder =
+        Jackson2ObjectMapperBuilder()
+            .serializers(LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")))
+            .serializers(LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+            .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+}
