0cf3aa24a082213e2639eb5c8bddc25a23efd39d
이현준 <11dlguswns@naver.com>
2025-03-27 18:20:09 +0900

    refactor :: Util 클래스 생성없이 사용할 수 있도록 변경

--- motung-i-api/src/main/kotlin/kr/motung_i/backend/global/security/service/CustomOauth2Service.kt
+++ motung-i-api/src/main/kotlin/kr/motung_i/backend/global/security/service/CustomOauth2Service.kt
@@ -1,9 +1,9 @@
 package kr.motung_i.backend.global.security.service
 
-import kr.motung_i.backend.persistence.auth.user.type.OAuth2UserInfoUtil
 import kr.motung_i.backend.persistence.user.entity.User
 import kr.motung_i.backend.persistence.user.entity.enums.Provider
 import kr.motung_i.backend.persistence.user.repository.UserCustomRepository
+import kr.motung_i.backend.persistence.user.util.UserInfoUtil
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import org.springframework.security.oauth2.core.user.OAuth2User
@@ -11,14 +11,13 @@ import org.springframework.stereotype.Service
 
 @Service
 class CustomOauth2Service(
-    private val oAuth2UserInfo: OAuth2UserInfoUtil,
     private val userRepository: UserCustomRepository,
 ) : DefaultOAuth2UserService() {
     override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
         val userAttribute: Map<String, Any> = super.loadUser(userRequest).attributes
         val provider: String = userRequest.clientRegistration.registrationId
         val getEntity: User =
-            oAuth2UserInfo.getInfoByProvider(
+            UserInfoUtil.getInfoByProvider(
                 attribute = userAttribute,
                 provider = Provider.valueOf(provider.uppercase()),
             )
---
+++ motung-i-domain/src/main/kotlin/kr/motung_i/backend/persistence/user/util/UserInfoUtil.kt
@@ -0,0 +1,33 @@
+package kr.motung_i.backend.persistence.user.util
+
+import kr.motung_i.backend.persistence.user.entity.User
+import kr.motung_i.backend.persistence.user.entity.enums.Provider
+import kr.motung_i.backend.persistence.user.entity.enums.Role
+
+class UserInfoUtil {
+    companion object {
+        fun getInfoByProvider(
+            provider: Provider,
+            attribute: Map<String, Any>,
+        ): User =
+            when (provider) {
+                Provider.APPLE ->
+                    User(
+                        name =
+                        attribute["name"] as? String ?: "모툴",
+                        role = Role.ROLE_USER,
+                        email = attribute["email"].toString(),
+                        oauthId = attribute["sub"].toString(),
+                        provider = Provider.APPLE,
+                    )
+                Provider.GOOGLE ->
+                    User(
+                        name = attribute["name"].toString(),
+                        role = Role.ROLE_USER,
+                        email = attribute["email"].toString(),
+                        oauthId = attribute["sub"].toString(),
+                        provider = Provider.GOOGLE,
+                    )
+            }
+    }
+}
