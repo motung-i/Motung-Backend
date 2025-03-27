f7d022ce40961e3204f08030ac54cef14eb26e81
이현준 <11dlguswns@naver.com>
2025-03-27 23:34:06 +0900

    refactor :: 엔티티 ERD에 맞도록 수정

--- motung-i-domain/src/main/kotlin/kr/motung_i/backend/persistence/review/entity/Review.kt
+++ motung-i-domain/src/main/kotlin/kr/motung_i/backend/persistence/review/entity/Review.kt
@@ -1,9 +1,12 @@
 package kr.motung_i.backend.persistence.review.entity
 
+import jakarta.persistence.CollectionTable
 import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import kr.motung_i.backend.persistence.BaseEntity
 import kr.motung_i.backend.persistence.user.entity.User
@@ -16,9 +19,11 @@ import java.util.UUID
 class Review (
     @Id
     @UuidGenerator
+    @Column(name = "REVIEW_ID")
     val id: UUID? = null,
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "USER_ID")
     @OnDelete(action = OnDeleteAction.CASCADE)
     val user: User,
 
@@ -30,4 +35,9 @@ class Review (
 
     @Column(nullable = false)
     val description: String,
+
+    @ElementCollection
+    @CollectionTable(name="REVIEW_IMAGE_URL", joinColumns = [JoinColumn(name = "REVIEW_ID")])
+    @Column(name = "IMAGE_URL")
+    val imageUrls: List<String>,
 ) : BaseEntity()
