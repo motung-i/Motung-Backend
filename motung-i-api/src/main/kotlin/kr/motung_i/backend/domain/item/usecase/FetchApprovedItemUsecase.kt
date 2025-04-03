9e5475da95525bab51730982373d01fef6ad896e
이현준 <11dlguswns@naver.com>
2025-04-03 17:15:20 +0900

    refactor :: 조회 API readOnly 추가

--- motung-i-api/src/main/kotlin/kr/motung_i/backend/domain/item/usecase/FetchApprovedItemUsecase.kt
+++ motung-i-api/src/main/kotlin/kr/motung_i/backend/domain/item/usecase/FetchApprovedItemUsecase.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 class FetchApprovedItemUsecase(
     private val itemCustomRepository: ItemCustomRepository,
 ) {
