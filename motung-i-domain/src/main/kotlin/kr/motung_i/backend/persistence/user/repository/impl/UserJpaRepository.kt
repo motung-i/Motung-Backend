7d392002670f33798f1912bd0b78e888b82b96eb
Jihyuk Lee <lead@sver.dev>
2025-05-27 09:32:02 +0900

    delete :: 불필요한 repository 제거

--- motung-i-domain/src/main/kotlin/kr/motung_i/backend/persistence/user/repository/impl/UserJpaRepository.kt
+++ motung-i-domain/src/main/kotlin/kr/motung_i/backend/persistence/user/repository/impl/UserJpaRepository.kt
@@ -8,11 +8,15 @@ import java.util.UUID
 interface UserJpaRepository : JpaRepository<User, UUID> {
     fun findByOauthId(oauthId: String): User?
 
-    fun findByEmail(email: String): User?
-
     fun existsByNickname(name: String): Boolean
 
-    fun existsByOauthIdAndProvider(oauthId: String, provider: Provider): Boolean
+    fun existsByOauthIdAndProvider(
+        oauthId: String,
+        provider: Provider,
+    ): Boolean
 
-    fun findByOauthIdAndProvider(oauthId: String, provider: Provider): User?
+    fun findByOauthIdAndProvider(
+        oauthId: String,
+        provider: Provider,
+    ): User?
 }
