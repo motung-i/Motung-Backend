11353970181feab90bac0ff83a00dc301724445a
Jihyuk Lee <lead@sver.dev>
2025-05-27 17:57:32 +0900

    delete :: 프로덕션 여부 Boolean 제거

--- motung-i-api/src/main/kotlin/kr/motung_i/backend/domain/notification/presentation/dto/request/SendNotificationRequest.kt
+++ motung-i-api/src/main/kotlin/kr/motung_i/backend/domain/notification/presentation/dto/request/SendNotificationRequest.kt
@@ -3,7 +3,6 @@ package kr.motung_i.backend.domain.notification.presentation.dto.request
 import java.util.UUID
 
 data class SendNotificationRequest(
-    val onProduction: Boolean,
     val title: String,
     val body: String,
     val targetUserId: UUID?,
