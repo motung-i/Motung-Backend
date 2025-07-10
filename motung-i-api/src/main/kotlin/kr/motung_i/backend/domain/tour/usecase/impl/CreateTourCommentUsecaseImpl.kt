f6f2940e9030325d1e30f9f28bc2a260f4ece546
이현준 <11dlguswns@naver.com>
2025-07-10 10:19:07 +0900

    refactor :: 여행 확정후에만 여행 장소 추천을 할수 있도록 수정

--- motung-i-api/src/main/kotlin/kr/motung_i/backend/domain/tour/usecase/impl/CreateTourCommentUsecaseImpl.kt
+++ motung-i-api/src/main/kotlin/kr/motung_i/backend/domain/tour/usecase/impl/CreateTourCommentUsecaseImpl.kt
@@ -1,13 +1,10 @@
 package kr.motung_i.backend.domain.tour.usecase.impl
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import kr.motung_i.backend.domain.tour.usecase.CreateTourCommentUsecase
 import kr.motung_i.backend.domain.user.usecase.FetchCurrentUserUsecase
 import kr.motung_i.backend.global.exception.CustomException
 import kr.motung_i.backend.global.exception.enums.CustomErrorCode
 import kr.motung_i.backend.global.third_party.open_ai.OpenAiAdapter
-import kr.motung_i.backend.global.third_party.open_ai.dto.OpenAiRecommendation
 import kr.motung_i.backend.persistence.tour.repository.TourRepository
 import kr.motung_i.backend.persistence.tour_comment.entity.TourComment
 import kr.motung_i.backend.persistence.tour_comment.repository.TourCommentRepository
@@ -16,7 +13,8 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
-class CreateTourCommentUsecaseImpl(
+class
+CreateTourCommentUsecaseImpl(
     private val openAiAdapter: OpenAiAdapter,
     private val tourCommentRepository: TourCommentRepository,
     private val tourRepository: TourRepository,
@@ -25,8 +23,8 @@ class CreateTourCommentUsecaseImpl(
     override fun execute() {
         val currentUser = fetchCurrentUserUsecase.execute()
 
-        val tour = tourRepository.findByUser(currentUser)
-            ?: throw CustomException(CustomErrorCode.NOT_FOUND_TOUR)
+        val tour = tourRepository.findByUserAndIsActive(currentUser, true)
+            ?: throw CustomException(CustomErrorCode.NOT_ACTIVATED_TOUR)
 
         if (tourCommentRepository.existsByTour(tour)) {
             throw CustomException(CustomErrorCode.ALREADY_EXISTS_TOUR_COMMENT)
